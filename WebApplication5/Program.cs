0a4d8954f21a2741fe713b209b47e3ebde168baf
0vrong <zaharhairyl@gmail.com>
2025-03-07 23:54:11 +0500

    Добавьте файлы проекта.

---
+++ WebApplication5/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+List<Pills> repo = [];
+
+app.MapGet("/", () => repo);
+app.MapPost("/", (CreateTablesDTO dto) =>
+{
+    var pills = new Pills
+    {
+        id = Guid.NewGuid(),
+        name = dto.name,
+        mg = dto.mg,
+        price = dto.price,
+        DateNow = DateTime.Now,
+        DateEnd = DateTime.Now.AddYears(3)
+    };
+    repo.Add(pills);
+});
+app.MapPut("/", ([FromQuery]Guid id,UpdateTablesDTO dto) =>
+{
+    Pills buffer = repo.Find(x => x.id == id);
+    buffer.name = dto.name;
+    buffer.mg = dto.mg;
+    buffer.price = dto.price;
+});
+app.MapDelete("/", ([FromQuery] Guid id) =>
+{
+    Pills buffer = repo.Find(x => x.id == id);
+    repo.Remove(buffer);
+});
+app.Run();
+
+class Pills
+{
+    public Guid id { get; set; }
+    public string? name { get; set; }
+    public int? mg { get; set; }
+    public double? price { get; set; }
+    public DateTime DateNow { get; set; }
+    public DateTime DateEnd { get; set; }
+
+}
+
+record class CreateTablesDTO(string name, int mg, double price);
+record class UpdateTablesDTO (string name, int mg, double price);
